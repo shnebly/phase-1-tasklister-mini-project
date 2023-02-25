c841da4ab03dd8b9e194b1ed2fe448542036045b
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-24 20:35:27 -0700

    changed remove function to include parent of btn i.e. the item

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
 function addToDo(item){
   let p = document.createElement('p')
   let btn = document.createElement('button')
-  btn.addEventListener('click', function(){
-    btn.remove()
+  btn.addEventListener('click', function(e){
+    e.target.parentNode.remove() // here e.target refers to the clicked target and since we are wanting to remove itself and the task (the parent), we grab the parentNode before calling remove() - this will remove the parent and in turn the child(btn)
   })
   p.textContent = `${item}  `
   btn.textContent = ' X '
@@ -18,5 +18,4 @@ function addToDo(item){
   document.querySelector('#tasks').appendChild(p)
 }
 
-// need to add function to X btn
 // need to remove previos entry from task input 
