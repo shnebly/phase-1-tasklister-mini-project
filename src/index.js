aed90cc1e3cde25c879856eba24d57632139d51e
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-24 20:23:23 -0700

    changed HTML context to include _ instead of -

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.new_task_description.value);
+  });
 });
+
+function addToDo(item){
+  let p = document.createElement('p')
+  p.textContent = item
+  console.log(p)
+}
